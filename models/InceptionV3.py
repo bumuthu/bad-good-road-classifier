ee3c0194e42972df0eff85afd74a06600af2700c
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-02 19:12:58 +0530

    Inceptionv3

--- models/InceptionV3.py
+++ models/InceptionV3.py
@@ -36,8 +36,8 @@ class InceptionV3Classifier:
         paths_train, paths_test, y_train, y_test = train_test_split(paths, y, test_size=self.test_ratio,
                                                                     random_state=42)
 
-        train_path_dict = [{"filename": paths_train[i], "class": y_train[i]} for i in range(len(paths_train))]
-        test_path_dict = [{"filename": paths_test[i], "class": y_test[i]} for i in range(len(paths_test))]
+        train_path_dict = [{"filename": paths_train[i], "class": str(y_train[i])} for i in range(len(paths_train))]
+        test_path_dict = [{"filename": paths_test[i], "class": str(y_test[i])} for i in range(len(paths_test))]
 
         self.train_df = pd.DataFrame.from_dict(train_path_dict)
         self.test_df = pd.DataFrame.from_dict(test_path_dict)
