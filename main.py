25fef58884bf3c6bae7922b696796a276bde0d51
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-02 22:16:04 +0530

    With 5 models

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from models.inceptionv3 import InceptionV3Classifier
+from models.vgg16 import VGG16Classifier
+from models.vgg19 import VGG19Classifier
+from models.resnet50 import ResNet15Classifier
+from models.xception import XceptionClassifier
 from preprocessing import DataPreprocessing
 
 if __name__ == "__main__":
@@ -8,11 +12,15 @@ if __name__ == "__main__":
     test_ratio = 0.3
 
     preproc = DataPreprocessing(data_path, test_ratio)
-
     preproc.prepare_image_path_df()
 
-    inceptionv3_model = InceptionV3Classifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs)
+    vgg16_model = VGG16Classifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs)
+    vgg16_model.prepare_data_generator()
+    vgg16_model.make_vgg16_model()
+    vgg16_model.train_model()
+    vgg16_model.evaluate_model()
 
+    inceptionv3_model = InceptionV3Classifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs)
     inceptionv3_model.prepare_data_generator()
     inceptionv3_model.make_inceptionv3_model()
     inceptionv3_model.train_model()
