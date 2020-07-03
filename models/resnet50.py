6852a53a504f560c1906ca7a3fa63438642996a3
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-03 19:29:39 +0530

    With 5 models

--- models/resnet50.py
+++ models/resnet50.py
@@ -67,8 +67,9 @@ class ResNet50Classifier:
 
         add_model = Sequential()
         add_model.add(base_model)
+        add_model.add(Dropout(0.2))
+        add_model.add(Dense(1024, activation='relu'))
         add_model.add(GlobalAveragePooling2D())
-        add_model.add(Dropout(0.5))
         add_model.add(Dense(2, activation='softmax'))
 
         self.model = add_model
