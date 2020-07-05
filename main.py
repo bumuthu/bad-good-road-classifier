6bc91184580833ddbd718b5a2c8131d6b32f4630
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-05 17:49:28 +0530

    With 5 models

--- main.py
+++ main.py
@@ -48,14 +48,14 @@ if __name__ == "__main__":
     # train_model(inceptionv3_model)
     # train_model(vgg19_model)
     # train_model(vgg16_model)
-    # train_model(inceptionresnetv2_model)
+    train_model(inceptionresnetv2_model)
 
-    evaluate_model(resnet50_model)
-    evaluate_model(xception_model)
-    evaluate_model(inceptionv3_model)
-    evaluate_model(vgg19_model)
-    evaluate_model(inceptionresnetv2_model)
-    evaluate_model(vgg16_model)
+    # evaluate_model(resnet50_model)
+    # evaluate_model(xception_model)
+    # evaluate_model(inceptionv3_model)
+    # evaluate_model(vgg19_model)
+    # evaluate_model(inceptionresnetv2_model)
+    # evaluate_model(vgg16_model)
 
 
 
