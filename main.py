4bab9d5fbd1dd6e71c8e8e90cf9e70be0fda2393
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-20 22:15:52 +0530

    With evaluation with epochs

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from preprocessing import DataPreprocessing
 if __name__ == "__main__":
 
     # number of epochs
-    epochs = 3
+    epochs = 30
 
     # select the path to data directory
     # '<path to dataset>'
