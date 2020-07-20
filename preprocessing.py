788b6c970cb966c3f9095f6a063d4c5ab5c2c389
Bumuthu <bumuthu.dilshan@gmail.com>
2020-07-20 21:09:51 +0530

    With evaluation with epochs

--- preprocessing.py
+++ preprocessing.py
@@ -23,7 +23,7 @@ class DataPreprocessing:
             y.append(1)
 
         paths_train, paths_test, y_train, y_test = train_test_split(paths, y, test_size=self.test_ratio,
-                                                                    shuffle=True)
+                                                                    random_state=42, shuffle=True)
 
         train_path_dict = [{"filename": paths_train[i], "class": str(y_train[i])} for i in range(len(paths_train))]
         test_path_dict = [{"filename": paths_test[i], "class": str(y_test[i])} for i in range(len(paths_test))]
