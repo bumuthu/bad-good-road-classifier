05bcaf28266239f8aa06071cd8907a7a03a5d284
Bumuthu <bumuthu.dilshan@gmail.com>
2020-08-20 14:16:39 +0530

    Phase 03 started

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     # select the path to data directory
     # '<path to dataset>'
-    data_path = '/home/bumuthudilshanhhk/Downloads/dataset/'
+    data_path = '/home/bumuthudilshanhhk/Downloads/dataset-new/'
 
     # data split ratio for training and testing
     test_ratio = 0.3
