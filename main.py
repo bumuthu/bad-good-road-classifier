d6ab5983f79d4c16e49b756c46327bece8042aef
Bumuthu <bumuthu.dilshan@gmail.com>
2020-08-29 09:41:32 +0530

    3 class classification

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
 
     # train_model(resnet50_model)
     # train_model(xception_model)
-    train_model(inceptionv3_model)
-    train_model(vgg19_model)
+    # train_model(inceptionv3_model)
+    # train_model(vgg19_model)
     train_model(inceptionresnetv2_model)
 
     evaluate_model(resnet50_model)
