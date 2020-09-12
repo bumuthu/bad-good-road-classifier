22d8b95523fdd74caf5cfc345c13a61ef449f6b3
Bumuthu <bumuthu.dilshan@gmail.com>
2020-09-13 01:55:55 +0530

    noise adding for 3 class classification (i.e. good, crack, pothole)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from models.inceptionv3 import InceptionV3Classifier
 from models.vgg19 import VGG19Classifier
 from models.resnet50 import ResNet50Classifier
@@ -38,6 +39,34 @@ if __name__ == "__main__":
         model.create_model()
         model.evaluate_model()
 
+    noise_levels = [0.05, 0.1, 0.2]
+
+    results = {"resnet": [], "xception": [], "inceptionv3": [], "inceptionresnetv2": []}
+
+    # Testing for noisy images
+    for func in ['sp', 'rand']:
+        for level in noise_levels:
+            preproc = DataPreprocessing(data_path, test_ratio)
+            preproc.prepare_image_path_df(func, level)
+
+            inceptionv3_model = InceptionV3Classifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs,
+                                                      batch_size)
+            resnet50_model = ResNet50Classifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs, batch_size)
+            xception_model = XceptionClassifier(preproc.train_df, preproc.test_df, preproc.y_test, epochs, batch_size)
+            inceptionresnetv2_model = InceptionResNetV2Classifier(preproc.train_df, preproc.test_df, preproc.y_test,
+                                                                  epochs, batch_size)
+
+            noise = func + str(level)
+            noise_attr = func + '-level-' + str(level)
+
+            results["resnet"].append({noise_attr: evaluate_model(resnet50_model, noise)})
+            results["xception"].append({noise_attr: evaluate_model(xception_model, noise)})
+            results["inceptionv3"].append({noise_attr: evaluate_model(inceptionv3_model, noise)})
+            results["inceptionresnetv2"].append({noise_attr: evaluate_model(inceptionresnetv2_model, noise)})
+
+            with open('./history/noise.json', 'w') as f:
+                json.dump(results, f)
+
     # uncomment followings for training
 
     # train_model(resnet50_model)
@@ -45,12 +74,6 @@ if __name__ == "__main__":
     # train_model(inceptionv3_model)
     # train_model(vgg19_model)
     # train_model(inceptionresnetv2_model)
-
-    evaluate_model(resnet50_model)
-    evaluate_model(xception_model)
-    evaluate_model(inceptionv3_model)
-    # evaluate_model(vgg19_model)
-    evaluate_model(inceptionresnetv2_model)
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
-
+import cv2
+import numpy as np
+from dtype import img_as_float
 
 class DataPreprocessing:
 
@@ -11,7 +13,172 @@ class DataPreprocessing:
         self.ROWS = 139
         self.COLS = 139
 
-    def prepare_image_path_df(self):
+    def random_noise(self, image, mode='gaussian', seed=None, clip=True, **kwargs):
+
+        mode = mode.lower()
+
+        # Detect if a signed image was input
+        if image.min() < 0:
+            low_clip = -1.
+        else:
+            low_clip = 0.
+
+        image = img_as_float(image)
+        if seed is not None:
+            np.random.seed(seed=seed)
+
+        allowedtypes = {
+            'gaussian': 'gaussian_values',
+            'localvar': 'localvar_values',
+            'poisson': 'poisson_values',
+            'salt': 'sp_values',
+            'pepper': 'sp_values',
+            's&p': 's&p_values',
+            'speckle': 'gaussian_values'}
+
+        kwdefaults = {
+            'mean': 0.,
+            'var': 0.01,
+            'amount': 0.05,
+            'salt_vs_pepper': 0.5,
+            'local_vars': np.zeros_like(image) + 0.01}
+
+        allowedkwargs = {
+            'gaussian_values': ['mean', 'var'],
+            'localvar_values': ['local_vars'],
+            'sp_values': ['amount'],
+            's&p_values': ['amount', 'salt_vs_pepper'],
+            'poisson_values': []}
+
+        for key in kwargs:
+            if key not in allowedkwargs[allowedtypes[mode]]:
+                raise ValueError('%s keyword not in allowed keywords %s' %
+                                 (key, allowedkwargs[allowedtypes[mode]]))
+
+        # Set kwarg defaults
+        for kw in allowedkwargs[allowedtypes[mode]]:
+            kwargs.setdefault(kw, kwdefaults[kw])
+
+        if mode == 'gaussian':
+            noise = np.random.normal(kwargs['mean'], kwargs['var'] ** 0.5,
+                                     image.shape)
+            out = image + noise
+
+        elif mode == 'localvar':
+            # Ensure local variance input is correct
+            if (kwargs['local_vars'] <= 0).any():
+                raise ValueError('All values of `local_vars` must be > 0.')
+
+            # Safe shortcut usage broadcasts kwargs['local_vars'] as a ufunc
+            out = image + np.random.normal(0, kwargs['local_vars'] ** 0.5)
+
+        elif mode == 'poisson':
+            # Determine unique values in image & calculate the next power of two
+            vals = len(np.unique(image))
+            vals = 2 ** np.ceil(np.log2(vals))
+
+            # Ensure image is exclusively positive
+            if low_clip == -1.:
+                old_max = image.max()
+                image = (image + 1.) / (old_max + 1.)
+
+            # Generating noise for each unique value in image.
+            out = np.random.poisson(image * vals) / float(vals)
+
+            # Return image to original range if input was signed
+            if low_clip == -1.:
+                out = out * (old_max + 1.) - 1.
+
+        elif mode == 'salt':
+            # Re-call function with mode='s&p' and p=1 (all salt noise)
+            out = self.random_noise(image, mode='s&p', seed=seed,
+                               amount=kwargs['amount'], salt_vs_pepper=1.)
+
+        elif mode == 'pepper':
+            # Re-call function with mode='s&p' and p=1 (all pepper noise)
+            out = self.random_noise(image, mode='s&p', seed=seed,
+                               amount=kwargs['amount'], salt_vs_pepper=0.)
+
+        elif mode == 's&p':
+            out = image.copy()
+            p = kwargs['amount']
+            q = kwargs['salt_vs_pepper']
+            flipped = np.random.choice([True, False], size=image.shape,
+                                       p=[p, 1 - p])
+            salted = np.random.choice([True, False], size=image.shape,
+                                      p=[q, 1 - q])
+            peppered = ~salted
+            out[flipped & salted] = 1
+            out[flipped & peppered] = low_clip
+
+        elif mode == 'speckle':
+            noise = np.random.normal(kwargs['mean'], kwargs['var'] ** 0.5,
+                                     image.shape)
+            out = image + image * noise
+
+        # Clip back to original range, if necessary
+        if clip:
+            out = np.clip(out, low_clip, 1.0)
+
+        return out
+
+    def salt_pepper(self, image, noise_level):
+        noise = self.random_noise(image, mode='s&p', amount=noise_level)
+        out = np.array(255 * noise, dtype=np.uint8)
+        return out
+
+    def localvar_noise(self, image, noise_level):
+        out = image + noise_level * image.std() * np.random.random(image.shape)
+        return out
+
+    # Save sault and pepper noisy images in a datset directory
+    def save_sp_noises(self, paths):
+        noise_levels = [0.05, 0.1, 0.2, 0.3]
+        for n in noise_levels:
+            for p in paths:
+                path = p["filename"]
+                img = cv2.imread(path)
+                img_new = self.salt_pepper(img, n)
+                new_path = self.data_dir + 'sp' + str(n) + '/' + os.path.basename(path)
+                cv2.imwrite(new_path, img_new)
+
+    # Save random noisy images in a datset directory
+    def save_rand_noises(self, paths):
+        noise_levels = [0.05, 0.1, 0.2, 0.3]
+        for n in noise_levels:
+            for p in paths:
+                path = p["filename"]
+                img = cv2.imread(path)
+                img_new = self.salt_pepper(img, n)
+                new_path = self.data_dir + 'rand' + str(n) + '/' + os.path.basename(path)
+                cv2.imwrite(new_path, img_new)
+
+    # Read sault and pepper noisy images
+    def add_sp_noises(self, paths, level):
+        new_dict = []
+        n = level
+
+        for p in paths:
+            path = p["filename"]
+            new_path = self.data_dir + 'sp' + str(n) + '/' + os.path.basename(path)
+
+            new_dict.append({"filename": new_path, "class": p["class"]})
+        return new_dict
+
+    # Read random noisy images
+    def add_rand_noises(self, paths, level):
+
+        new_dict = []
+        n = level
+
+        for p in paths:
+            path = p["filename"]
+            new_path = self.data_dir + 'rand' + str(n) + '/' + os.path.basename(path)
+
+            new_dict.append({"filename": new_path, "class": p["class"]})
+        return new_dict
+
+    def prepare_image_path_df(self, func, level):
         paths = []
         y = []
 
@@ -30,6 +197,11 @@ class DataPreprocessing:
 
         train_path_dict = [{"filename": paths_train[i], "class": str(y_train[i])} for i in range(len(paths_train))]
         test_path_dict = [{"filename": paths_test[i], "class": str(y_test[i])} for i in range(len(paths_test))]
+
+        if func == 'sp':
+            test_path_dict = self.add_sp_noises(test_path_dict, level)
+        elif func == 'rand':
+            test_path_dict = self.add_rand_noises(test_path_dict, level)
 
         self.train_df = pd.DataFrame.from_dict(train_path_dict)
         self.test_df = pd.DataFrame.from_dict(test_path_dict)
